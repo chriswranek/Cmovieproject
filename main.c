5309930e5f1fc321753f084557fb9ee89c2cb601
chriswranek <69898877+chriswranek@users.noreply.github.com>
2020-08-21 13:07:52 +0100

    Delete main.c

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include "movieDatabase.h"
-
-int main() {
-    movieDatabase *mdb = mdb_new();
-    char *input = "films.txt";
-    mdb_loadFromFile(input, mdb);
-}
